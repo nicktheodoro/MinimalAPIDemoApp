51c32c0bbb3a377b2a115af5e285aa81e75812fb
nicktheodoro <nicolastheodoro@gmail.com>
2021-12-15 17:25:20 -0300

    Add project files.

---
+++ DataAccess/DbAccess/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿
+namespace DataAccess.DbAccess;
+
+public interface ISqlDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U paramters, string connectionId = "Default");
+    Task SaveData<T>(string storedProcedure, T paramters, string connectionId = "Default");
+}
